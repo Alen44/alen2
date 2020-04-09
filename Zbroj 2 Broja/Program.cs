abdde76b156852a93291e1a573b0d2c461ca667a
Alen <alen.arambasic94@gmail.com>
2020-04-09 20:17:51 +0200

    zbroj 2 broja

---
+++ Zbroj 2 Broja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zbroj_2_Broja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prvi broj");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Rezultat je " + c); 
+        }
+    }
+}
