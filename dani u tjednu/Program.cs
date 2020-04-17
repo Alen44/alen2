ee5b13ae1d4479c5d356d9229cf19dacffb9eb00
Alen <alen.arambasic94@gmail.com>
2020-04-17 12:22:50 +0200

    program koji ispisuje koji je dan radni, a koji neradni

---
+++ dani u tjednu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dani_u_tjednu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi dan u tjednu");
+            Console.WriteLine("PON UTO SRI CET PET SUB NED");
+
+            String dan = Console.ReadLine();
+
+            switch (dan)
+            {
+                case "PON":
+                case "UTO":
+                case "SRI":
+                case "CET":
+                case "PET": Console.WriteLine("Radni dan"); break;
+
+                case "SUB":
+                case "NED": Console.WriteLine("Neradni dan"); break;
+                default: Console.WriteLine("Nepoznati dan?!"); break;
+            }
+        }
+    }
+}
